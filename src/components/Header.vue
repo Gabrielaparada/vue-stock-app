2456943d21b56ca7cfd1820ad321b9e834f960de
gparada <gparada@carltonone.com>
2021-01-11 11:02:44 -0500

    added vue resource and firebase

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
         <strong class='navbar-text navbar-right'>Funds: {{ funds | currency}}</strong>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="#" @click="endDay">End Day</a></li>
-          <li class="dropdown">
+          <li  @click="isDropdownOpen = !isDropdownOpen " class="dropdown" :class=" {'open' : isDropdownOpen}">
             <a class="dropdown-toggle"
                role="button"
                data-toggle="dropdown"
@@ -20,7 +20,7 @@
                href="#"
                aria-expanded="false">Save & Load <span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><a href="#">Save Data</a></li>
+              <li><a href="#" @clik="saveData">Save Data</a></li>
               <li><a href="#">Another action</a></li>
             </ul>
           </li>
@@ -34,19 +34,34 @@
 <script>
 import {mapActions} from 'vuex'
   export default {
-    computed:{
-      funds(){
+    data() {
+      return {
+        isDropdownOpen: false
+      }
+    },
+    computed: {
+      funds() {
         return this.$store.getters.funds
       }
     },
-    methods:{
+    methods: {
       ...mapActions([
-          // if you want to keep the original name of the action you need to pass an array
+        // if you want to keep the original name of the action you need to pass an array
         'randomnizeStock'
       ]),
-      endDay(){
-        console.log('hit')
+      endDay() {
+        console.log(this.$store)
         this.randomnizeStock();
+      },
+      saveData() {
+        const data = {
+          funds: this.$store.getters.funds,
+          stockPortfolio: this.$store.getters.stockPortfolio,
+          stocks: this.$store.getters.stocks
+        };
+        //using put will overwrite the ol data
+        // //data.json is the node iof the file we want to create
+        // this.$http.put('data.json', data)
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes.js'
 import store from './store/store.js'
+import VueResource from 'vue-resource'
+
 
 Vue.filter('currency', (value)=>{
   return '$' + value.toLocaleString();
 })
+Vue.use(VueResource)
+
+Vue.http.optios.root = 'https://vuejs-stock-trader-5817f-default-rtdb.firebaseio.com/'
+
 Vue.use(VueRouter)
 const router = new VueRouter({
   mode: 'history',
